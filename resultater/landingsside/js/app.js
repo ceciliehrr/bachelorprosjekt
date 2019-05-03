c9f1f67d37116d89975c59a4ef92bef99610602a
ceciliehrr <ceciliehrr@gmail.com>
2019-05-03 14:02:00 +0200

    resultater, landingsside

---
+++ resultater/landingsside/js/app.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.11.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAl0BeCv9i-JUjlMRw0ZPswLdcDEuO1ijA",
+    authDomain: "bachelorprosjekt-3ea68.firebaseapp.com",
+    databaseURL: "https://bachelorprosjekt-3ea68.firebaseio.com",
+    projectId: "bachelorprosjekt-3ea68",
+    storageBucket: "bachelorprosjekt-3ea68.appspot.com",
+    messagingSenderId: "354920665801"
+  };
+  firebase.initializeApp(config);
+</script>
