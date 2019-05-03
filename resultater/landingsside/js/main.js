305cd3a4897bf44341a1b9754fecccb7746d1ab4
ceciliehrr <ceciliehrr@gmail.com>
2019-05-03 20:43:06 +0200

    resultater

--- resultater/landingsside/js/main.js
+++ resultater/landingsside/js/main.js
@@ -16,18 +16,18 @@ var dbFolk = '{"folkeregisteret":[' +
 '{"personID":"1", "fornavn":"Bjørn", "mellomnavn":" ", "etternavn":"Bar"},'+
 '{"personID":"2", "fornavn":"Lise", "mellomnavn":" ", "etternavn":"Nilsson"},'+
 '{"personID":"3", "fornavn":"Evelyn", "mellomnavn":" ", "etternavn":"Hansen"},'+
-'{"personID":"4", "fornavn":"Knut", "mellomnavn":"E P ", "etternavn":"Tråden"},'+
+'{"personID":"4", "fornavn":"Knut", "mellomnavn":"E. P. ", "etternavn":"Tråden"},'+
 '{"personID":"5", "fornavn":"Per", "mellomnavn":" ", "etternavn":"Nyse"},'+
 '{"personID":"6", "fornavn":"Eli", "mellomnavn":" ", "etternavn":"Hove"} ]}';
 
 var dbOrg ='{"enhetsregisteret": [' +
-'{"orgNr":"990039410","foretaksnavn":"Bjørn Pub" ,"Daglig leder":"Bjørn Bar" ,"bransje":"Drift av pub/serveringssted og annen virksomhet i tilknytning til  dette. "} ]}';
+'{"orgNr":"990039410","foretaksnavn":"Bjørn Pub" ,"Daglig leder":"Bjørn Bar" ,"bransje":"Pubeier "} ]}';
 
 
 innF = JSON.parse(dbFolk);
 innI = JSON.parse(dbIn);
 //innA = JSON.parse(dbBr);
-//innO = JSON.parse(dbOrg);
+innO = JSON.parse(dbOrg);
 //Bjørn sine data inn i brukergrensesnittet
 document.getElementById("fornavnTittel").innerHTML = innF.folkeregisteret[0].fornavn;
 document.getElementById("navn").innerHTML = innF.folkeregisteret[0].fornavn + " " +  innF.folkeregisteret[0].mellomnavn + " " + innF.folkeregisteret[0].etternavn;
@@ -36,6 +36,8 @@ document.getElementById("kontaktperson").innerHTML = innF.folkeregisteret[3].for
 
 document.getElementById("telefon").innerHTML = innI.innbygger[0].telefon;
 document.getElementById("mail").innerHTML = innI.innbygger[0].mail;
+
+document.getElementById("arbeid").innerHTML = innO.enhetsregisteret[0].bransje;
 
 innM = JSON.parse(dbMatr);
 document.getElementById("adresse").innerHTML = innM.matrikkelen[0].adressenavn + " " + innM.matrikkelen[0].adressenummer;
