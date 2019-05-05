829c9a3f4412e5ae98f8a1bb47b75d4876d41f80
ceciliehrr <ceciliehrr@gmail.com>
2019-05-05 22:37:18 +0200

    skjemaendring

--- resultater/skjema/js/main.js
+++ resultater/skjema/js/main.js
@@ -129,9 +129,9 @@ function showTab(n) {
     document.getElementById("prevBtn").style.display = "inline";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("nextBtn").innerHTML = "Send";
   } else {	
-    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("nextBtn").innerHTML = "Neste";
   }
   //... and run a function that will display the correct step indicator:
   fixStepIndicator(n)
